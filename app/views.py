de9436a4788952f7330d3e0dd0e5e2061443f06b
Kevin Moody <80837141+moodykevin@users.noreply.github.com>
2023-11-17 14:47:51 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -11,4 +11,22 @@ def getProviders(request):
     with open(json_file_path, 'r') as file:
         data = json.load(file)
 
+    if 'search' in request.GET:
+        q = request.GET
+        print(q)
+        if 'fff' in request.GET['SERCHTEST1']:
+            print("HERE")
+        
+        return render(request, 'app/search.html', {'data': data})
     return render(request, 'app/index.html', {'data': data})
+
+def searchProviders(request):
+    json_file_path = os.path.join(settings.BASE_DIR, 'app/static/app/providers.json')
+    with open(json_file_path, 'r') as file:
+        data = json.load(file)
+
+
+    if 'search' in request.GET:
+        return render(request, 'app/search.html', {'data': data})
+    
+    return render(request, 'app/search.html', {'data': data})
