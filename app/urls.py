758ce1738c4a8055b220c2f914cff27ef2ebeb9c
tclaughridge <tcl5tu@virginia.edu>
2023-11-17 15:05:35 -0500

    Modified urls.py

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'app'
 urlpatterns = [
     path('', views.getProviders, name='index'),
-    path("admin/", admin.site.urls),
-    path('search', views.searchProviders, name='search')
+    path('search/', views.searchProviders, name='search'),
+    path("admin/", admin.site.urls)
 ]
